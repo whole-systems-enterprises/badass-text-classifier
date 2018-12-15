ef692380654f2084bc3825d9de89349dfe745d7f
whole-systems-enterprises <whole.systems.enterprises@gmail.com>
2018-12-15 12:18:02 -0800

    Initial import.

---
+++ LSTM_train_validate_and_test.py
@@ -0,0 +1,151 @@
+#
+# load useful libraries
+#
+import pprint as pp
+import numpy as np
+import os
+import pickle
+import sys
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+#
+# load useful local libraries
+#
+import LSTM_utilities as ut
+
+#
+# user settings
+#
+output_directory = 'LSTM_output'
+x_train_filename = 'output/x_train.txt'
+y_train_filename = 'output/y_train.txt'
+x_val_filename = 'output/x_val.txt'
+y_val_filename = 'output/y_val.txt'
+x_test_filename = 'output/x_test.txt'
+y_test_filename = 'output/y_test.txt'
+
+output_scores_file = 'scores.pickled'
+
+GloVe_file = '/Users/emily/Desktop/data/NLP/GloVe/glove.6B.100d.txt'
+MAX_SEQUENCE_LENGTH = 1000
+
+layers = [1, 2, 3]
+number_of_cells_to_try = [10, 15, 30, 70, 128, 200]
+embeddings_to_try = ['learned']
+
+MODEL_CP_DIR = output_directory + '/checkpoints'
+TENSORBOARD_BASE_DIR = output_directory + '/tensorboard'
+
+epochs = 2
+
+#
+# crudely clear the way
+#
+os.system('rm -R ' + output_directory)
+os.system('mkdir ' + output_directory)
+os.system('rm -R ' + MODEL_CP_DIR)
+os.system('mkdir ' + MODEL_CP_DIR)
+os.system('mkdir ' + MODEL_CP_DIR + '/weights')
+os.system('mkdir ' + MODEL_CP_DIR + '/images')
+os.system('rm -R ' + TENSORBOARD_BASE_DIR)
+os.system('mkdir ' + TENSORBOARD_BASE_DIR)
+
+#
+# load files
+#
+x_train, y_train = ut.load_x_and_y_lists(x_train_filename, y_train_filename)
+x_val, y_val = ut.load_x_and_y_lists(x_val_filename, y_val_filename)
+x_test, y_test = ut.load_x_and_y_lists(x_test_filename, y_test_filename)
+
+#
+# make y lists NumPy arrays
+#
+y_train = np.asarray(y_train)
+y_val = np.asarray(y_val)
+y_test = np.asarray(y_test)
+
+#
+# tokenize
+#
+tokenizer = Tokenizer()
+texts_to_tokenize = x_train + x_val
+tokenizer.fit_on_texts(texts_to_tokenize)
+x_train = tokenizer.texts_to_sequences(x_train)
+x_train = pad_sequences(x_train, maxlen=MAX_SEQUENCE_LENGTH)
+x_val = tokenizer.texts_to_sequences(x_val)
+x_val = pad_sequences(x_val, maxlen=MAX_SEQUENCE_LENGTH)
+x_test = tokenizer.texts_to_sequences(x_test)
+x_test = pad_sequences(x_test, maxlen=MAX_SEQUENCE_LENGTH)
+
+#
+# create word index
+#
+word_index = tokenizer.word_index
+
+
+#
+# iterate through the embedding methods
+#
+for embedding_method in embeddings_to_try:
+
+    #
+    # create embeddings layer
+    #
+    if embedding_method == 'GloVe':
+        embeddings_index = ut.index_glove_embeddings(GloVe_file)
+        embedding_layer = ut.create_embedding_layer_GloVe(word_index, embeddings_index)
+    elif embedding_method == 'learned':
+        embedding_layer = ut.create_embedding_layer_learned(word_index)
+    else:
+        print('Unknown embedding method. Exiting.')
+        sys.exit(0)
+
+
+    #
+    # iterate through the layers
+    #
+    score_results = {}
+    for number_of_layers in layers:
+
+        score_results[number_of_layers] = {}
+
+        for number_of_cells in number_of_cells_to_try:
+
+            score_results[number_of_layers][number_of_cells] = None
+
+            #
+            # initialize model
+            #
+            model = ut.create_LSTM_model_framework(embedding_layer, number_of_layers, MAX_SEQUENCE_LENGTH, number_of_cells)
+
+            #
+            # train
+            #
+            results_file = ut.train_and_evaluate(x_train, y_train, x_val, y_val, model, number_of_layers, number_of_cells, embedding_method, MODEL_CP_DIR, TENSORBOARD_BASE_DIR, epochs=epochs)
+
+            #
+            # produce ROC
+            #
+            y_scores = model.predict(x_test)
+            roc_filename = results_file.replace('/weights/', '/images/') + '.png'
+            ut.compute_and_plot_ROC(y_test, y_scores, roc_filename)
+
+            #
+            # test
+            #
+            scores = model.evaluate(x_test, y_test, verbose=1)
+            score_results[number_of_layers][number_of_cells] = {
+                'scores' : scores,
+                'results_file' : results_file,
+                'ROC' : roc_filename,
+                }
+
+            print()
+            pp.pprint(score_results)
+            print()
+
+            with open(output_directory + '/' + output_scores_file, 'wb') as f:
+                pickle.dump(score_results, f)
+
---
+++ LSTM_utilities.py
@@ -0,0 +1,172 @@
+#
+# load useful libraries
+#
+import sys
+import os
+import numpy as np
+from keras.layers import Embedding
+from keras.layers import Dense, Input, LSTM, Activation, Dropout
+from keras.models import Model
+from keras.callbacks import TensorBoard, ModelCheckpoint
+from time import localtime, strftime
+from sklearn.metrics import roc_curve, auc
+
+#
+# compute ROC curve
+#
+# https://scikit-learn.org/stable/auto_examples/model_selection/plot_roc.html
+#
+def compute_and_plot_ROC(y_known, y_score, filename):
+
+    y_known = [int(x) for x in y_known]
+
+    # crude
+    y_score_list = []
+    for s in y_score:
+        y_score_list.append(s[0])
+
+    import matplotlib.pyplot as plt
+    fpr, tpr = roc_curve(y_known, y_score_list)
+    roc_auc = auc(fpr, tpr)
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=lw, label='ROC Curve (Area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.savefig(filename)
+    plt.close()
+
+#
+# Load lists of X and Y values
+#
+# Each line is an entry, the count of lines in the two files must be the same, and there is no header
+#
+def load_x_and_y_lists(x_list_filename, y_list_filename):
+    x_list = []
+    f = open(x_list_filename)
+    for line in f:
+        line = line.strip()
+        x_list.append(line)
+    f.close()
+
+    y_list = []
+    f = open(y_list_filename)
+    for line in f:
+        line = line.strip()
+        y_list.append(line)
+    f.close()
+
+    if len(x_list) != len(y_list):
+        print('Lists are not the same length. Exiting')
+        sys.exit(0)
+    if len(x_list) == 0:
+        print('Lists are empty. Exiting.')
+        sys.exit(0)
+
+    return x_list, y_list
+
+
+#
+# index GloVe embeddings
+#
+# This function is (very) slightly modified from that found in https://github.com/natel9178/CS230-news-bias/blob/master/train.py
+#
+def index_glove_embeddings(filename):
+    embeddings_index = {}
+    with open(filename) as f:
+        for line in f:
+            values = line.split()
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = coefs
+    return embeddings_index
+
+#
+# Create embedding layer
+#
+# This function is (very) slightly modified from that found in https://github.com/natel9178/CS230-news-bias/blob/master/train.py
+#
+def create_embedding_layer_GloVe(word_index, embeddings_index, EMBEDDING_DIM=100):
+    num_words = len(word_index) + 1
+
+    embedding_matrix = np.zeros((num_words, EMBEDDING_DIM))
+
+    for word, i in word_index.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+
+    return Embedding(
+        num_words,
+        EMBEDDING_DIM,
+        weights=[embedding_matrix],
+        trainable=False
+        )
+
+
+#
+# Create embedding layer
+#
+# This function is (very) slightly modified from that found in https://github.com/natel9178/CS230-news-bias/blob/master/train.py
+#
+def create_embedding_layer_learned(word_index, EMBEDDING_DIM=100):
+    num_words = len(word_index) + 1
+    return Embedding(
+        num_words,
+        EMBEDDING_DIM,
+        )
+
+
+#
+# create LSTM model framework
+#
+# This function is heavily modified from that found in https://github.com/natel9178/CS230-news-bias/blob/master/train.py
+#
+def create_LSTM_model_framework(embedding_layer, number_of_layers, MAX_SEQUENCE_LENGTH, number_of_cells):
+    sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+    embedded_sequences = embedding_layer(sequence_input)
+
+    X = embedded_sequences
+    for i in range(number_of_layers-1):
+        X = LSTM(number_of_cells, return_sequences=True)(X)
+        X = Dropout(0.2)(X)
+    X = LSTM(number_of_cells, return_sequences=False)(X)
+    X = Dropout(0.2)(X)
+    X = Dense(1)(X)
+    preds = Activation('sigmoid')(X)
+
+    return Model(sequence_input, preds)
+
+#
+# Train
+#
+# This function is heavily modified from that found in https://github.com/natel9178/CS230-news-bias/blob/master/train.py
+#
+def train_and_evaluate(x_train, y_train, x_val, y_val, model, number_of_layers, number_of_cells, embedding_method, MODEL_CP_DIR, TENSORBOARD_BASE_DIR, epochs=10):
+    model_type = 'lstm'
+
+    MODEL_CP_DIR = '{}layers-{}-cells-{}-embedding-method-{}-model-type-{}{}'.format(MODEL_CP_DIR + '/weights/', number_of_layers, number_of_cells, embedding_method, model_type, '-weights.best.hdf5')
+
+    model.compile(
+        loss='binary_crossentropy',
+        optimizer='adam',
+        metrics=['acc']
+        )
+
+    tensorboard = TensorBoard(log_dir=os.path.join(TENSORBOARD_BASE_DIR, "{}{}{}{}{}".format(number_of_layers, number_of_cells, embedding_method, model_type, strftime("%Y-%m-%d_%H-%M-%S", localtime()))))
+
+    checkpoint = ModelCheckpoint(MODEL_CP_DIR, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+
+    model.fit(
+        x_train,
+        y_train,
+        batch_size=128,
+        epochs=epochs,
+        validation_data=(x_val, y_val), verbose=1, callbacks=[tensorboard, checkpoint]
+        )
+
+    return MODEL_CP_DIR
