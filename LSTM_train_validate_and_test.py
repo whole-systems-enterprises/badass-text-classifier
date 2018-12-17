26040aed2a9890333d8fed191b8a389df6ab6032
whole-systems-enterprises <whole.systems.enterprises@gmail.com>
2018-12-16 17:21:13 -0800

    Configured the analysis of the IBC dataset to use GloVe embeddings.

--- LSTM_train_validate_and_test.py
+++ LSTM_train_validate_and_test.py
@@ -27,13 +27,21 @@ import LSTM_utilities as ut
 # x_test_filename = 'ideological-book-corpus/output/x_test.txt'
 # y_test_filename = 'ideological-book-corpus/output/y_test.txt'
 
-output_directory = 'LSTM_output_2016'
-x_train_filename = 'output/x_train.txt'
-y_train_filename = 'output/y_train.txt'
-x_val_filename = 'output/x_val.txt'
-y_val_filename = 'output/y_val.txt'
-x_test_filename = 'output/x_test.txt'
-y_test_filename = 'output/y_test.txt'
+output_directory = 'LSTM_output_IBC_GloVe'
+x_train_filename = 'ideological-book-corpus/output/x_train.txt'
+y_train_filename = 'ideological-book-corpus/output/y_train.txt'
+x_val_filename = 'ideological-book-corpus/output/x_val.txt'
+y_val_filename = 'ideological-book-corpus/output/y_val.txt'
+x_test_filename = 'ideological-book-corpus/output/x_test.txt'
+y_test_filename = 'ideological-book-corpus/output/y_test.txt'
+
+# output_directory = 'LSTM_output_2016'
+# x_train_filename = 'output/x_train.txt'
+# y_train_filename = 'output/y_train.txt'
+# x_val_filename = 'output/x_val.txt'
+# y_val_filename = 'output/y_val.txt'
+# x_test_filename = 'output/x_test.txt'
+# y_test_filename = 'output/y_test.txt'
 
 output_scores_file = 'scores.pickled'
 
@@ -42,7 +50,10 @@ MAX_SEQUENCE_LENGTH = 1000
 
 layers = [1, 2, 3]
 number_of_cells_to_try = [10, 15, 30, 70, 128, 200]
-embeddings_to_try = ['learned']
+
+#embeddings_to_try = ['learned']
+embeddings_to_try = ['GloVe']
+
 
 MODEL_CP_DIR = output_directory + '/checkpoints'
 TENSORBOARD_BASE_DIR = output_directory + '/tensorboard'
